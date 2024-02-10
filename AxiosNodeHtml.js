0caa9f137e65eb8a16968be81e6b328b73bc5413
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2024-02-10 18:20:22 +0700

    update Code

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -14,67 +14,69 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // server static files
 app.use(express.static(__dirname + '/public'));
 
-app.get ('/', async (req, res) => {
+app.get("/", async (req, res) => {
     try {
         const response = await axios.get(base_url + '/books');
-        res.render("books", {books: response.data });
-    } catch(err) {
+        res.render("books", { books: response.data });
+    } catch (err) {
         console.error(err);
-        res.status(5500).send('Error');
+        res.status(500).send('Error');
     }
 });
 
-app.get ('/book/id', async (req, res) => {
+// Corrected the route parameter name to :id
+app.get("/book/:id", async (req, res) => {
     try {
         const response = await axios.get(base_url + '/books/' + req.params.id);
-        res.render("book", {book: response.data });
-    } catch(err) {
+        res.render("book", { book: response.data });
+    } catch (err) {
         console.error(err);
-        res.status(5500).send('Error');
+        res.status(500).send('Error');
     }
 });
 
-app.get('/create', (req, res) => {
+app.get("/create", (req, res) => {
     res.render("create");
 });
 
-app.post('/create', async (req, res) => {
+app.post("/create", async (req, res) => {
     try {
         const data = { title: req.body.title, author: req.body.author };
         await axios.post(base_url + '/books', data);
-        res.redirect('/');
+        res.redirect("/");
     } catch (err) {
         console.error(err);
         res.status(500).send('Error');
     }
 });
 
-app.get('/update/:id', async (req, res) => {
+app.get("/update/:id", async (req, res) => {
     try {
         const response = await axios.get(
-        base_url + '/books/' + req.params.id);
-        res.render("update", {book: response.data});
+            base_url + '/books/' + req.params.id
+        );
+        res.render("update", { book: response.data });
     } catch (err) {
         console.error(err);
         res.status(500).send('Error');
     }
 });
 
-app.post('/update/:id', async (req, res) => {
+app.post("/update/:id", async (req, res) => {
     try {
         const data = { title: req.body.title, author: req.body.author };
         await axios.put(base_url + '/books/' + req.params.id, data);
-        res.redirect('/');
+        res.redirect("/");
     } catch (err) {
         console.error(err);
         res.status(500).send('Error');
     }
 });
 
-app.get('/delete/:id', async (req, res) => {
+app.get("/delete/:id", async (req, res) => {
     try {
         await axios.delete(base_url + '/books/' + req.params.id);
-        res.redirect('/');
+        res.redirect("/");
     } catch (err) {
         console.error(err);
         res.status(500).send('Error');
