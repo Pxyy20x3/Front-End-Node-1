362688acd9fdb08c8423822f194eaa85f5254252
Kittigan14 <kittigan1486@gmail.com>
2024-02-12 10:15:16 +0700

    update code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 var bodyParser = require('body-parser')
 
 // const base_url = "http://localhost:3000"
-const base_url = "http://10.104.12.13"
+const base_url = "http://node58185-sxdx2003.proen.app.ruk-com.cloud"
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
