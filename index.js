bac2eaaa5206affb806915f38bdbae6d9dd0ada2
Kittigan14 <kittigan1486@gmail.com>
2024-02-12 10:45:17 +0700

    update code

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 var bodyParser = require('body-parser')
-const path = require("path")
 
-const base_url = "http://localhost:3000";
+const base_url = "http://localhost:3000"
+//const base_url = "http://10.104.12.156"
 
-app.set("views",path.join(__dirname,"/public/views"))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:false}))
-
 
 app.use(express.static(__dirname + '/public'))
 
