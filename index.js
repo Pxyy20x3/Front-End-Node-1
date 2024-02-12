566c2f812b6b42c2a95ddb519ed94647de2cd183
Kittigan14 <kittigan1486@gmail.com>
2024-02-12 09:49:41 +0700

    update code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 var bodyParser = require('body-parser')
 
 // const base_url = "http://localhost:3000"
-const base_url = "10.104.12.156"
+const base_url = "http://10.104.12.156"
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
