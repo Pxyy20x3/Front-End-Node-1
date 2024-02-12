9651b83679b2c59fef2685da6744b15c953079dd
Kittigan14 <kittigan1486@gmail.com>
2024-02-12 09:24:46 +0700

    update code

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-
-
-
 const express = require('express')
 const axios = require('axios')
 const app = express()
 var bodyParser = require('body-parser')
 
-
-
 const base_url = "http://localhost:3000"
-
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
