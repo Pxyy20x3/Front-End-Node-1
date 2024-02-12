3e9419d5748e612c7f6ced06736f90be7b05afaa
Kittigan14 <kittigan1486@gmail.com>
2024-02-12 10:33:51 +0700

    update code

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express()
 var bodyParser = require('body-parser')
 const path = require("path")
 
-
 const base_url = "http://localhost:3000";
 
 app.set("views",path.join(__dirname,"/public/views"))
