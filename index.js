727711551a1484bf473d93bafd2d655d2bf224bd
Kittigan14 <kittigan1486@gmail.com>
2024-02-12 10:18:13 +0700

    update code

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-const path = require('path')
 var bodyParser = require('body-parser')
 
 // const base_url = "http://localhost:3000"
 const base_url = "http://node58185-sxdx2003.proen.app.ruk-com.cloud"
 
-app.set('view', path.join(__dirname, '/public/views'));
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:false}))
