d29366502067bedcd7e67765a1773a2e97ff416d
Kittigan14 <kittigan1486@gmail.com>
2024-02-22 14:37:28 +0700

    update code

--- src/AxiosNodeHtml.js
+++ src/AxiosNodeHtml.js
@@ -4,11 +4,8 @@ const app = express()
 var bodyParser = require('body-parser')
 
 const base_url = "http://localhost:3000";
-// const base_url = "http://10.104.7.149";
 
-
-
-//app.set('views', path.join(__dirname, "/public/views"))
+app.set('views', path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:false}))
