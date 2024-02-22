1e23bb8978ac2d66904538aa493e57149986fb6f
Kittigan14 <kittigan1486@gmail.com>
2024-02-22 16:08:23 +0700

    update code

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const path = require('path')
 
 const base_url = 'http://localhost:3000';
 
-//
 app.set('views', path.join(__dirname, '/public/views'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
