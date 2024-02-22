f68ef14c370d72420adc5886963e7c35b2c2e929
KittikarnIwai <kittigan1486@gmail.com>
2024-02-23 00:55:30 +0700

    update code

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 var bodyParser = require('body-parser')
 const path = require('path')
 
-const base_url = 'http://localhost:3000';
+// const base_url = 'http://localhost:3000';
+const base_url = 'node59005-env-4702943.proen.app.ruk-com.cloud';
 
 app.set('views', path.join(__dirname, '/public/views'))
 app.set('view engine', 'ejs')
